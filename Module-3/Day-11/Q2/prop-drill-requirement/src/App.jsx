0c790dff77e7252ff3309333c72d6f064da5c312
Sandhya Kummari <sandhyakummari2006@gmail.com>
2025-12-23 00:02:47 +0530

    useRef and props drilling

---
+++ Module-3/Day-11/Q2/prop-drill-requirement/src/App.jsx
@@ -0,0 +1,23 @@
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
+import Component3 from "./components/Component3";
+import Component4 from "./components/Component4";
+import Component5 from "./components/Component5";
+import Component6 from "./components/Component6";
+
+function App() {
+  return (
+    <div>
+      <Component1
+        a="Apple"
+        b="Ball"
+        c="Cat"
+        d="Dog"
+        e="Elephant"
+        f="Fish"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ Module-3/Day-11/Q2/prop-drill-requirement/src/components/Component1.jsx
@@ -0,0 +1,18 @@
+import Component1 from "./components/Component1";
+
+function App() {
+  return (
+    <div>
+      <Component1
+        a="Apple"
+        b="Ball"
+        c="Cat"
+        d="Dog"
+        e="Elephant"
+        f="Fish"
+      />
+    </div>
+  );
+}
+
+export default App;
